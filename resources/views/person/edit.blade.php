83bd48f19b3923452ba4624e431d12c45c181d59
Josua Perez <josuap63@gmail.com>
2023-06-19 19:41:49 -0400

    Implementing CRUD functions

---
+++ resources/views/person/edit.blade.php
@@ -0,0 +1,83 @@
+<x-app-layout>
+    <x-slot name="header">
+        <a href="{{ route('person.index') }}" class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('People') }}
+        </a>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
+                    <h3 class="font-semibold pb-5">Edit a person: {{$person->firstname}} {{$person->lastname}}</h3>
+
+                    <form action="{{ route('person.update', $person->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="grid grid-cols-1 sm:grid-cols-6 gap-x-6 gap-y-6">
+                            <span class="sm:col-span-3">
+                                <label class="py-4 block" for="firstname">First Name</label>
+                                <input
+                                    class="block w-full"
+                                    type="text"
+                                    name="firstname"
+                                    id="firstname"
+                                    value="{{old('firstname', $person->firstname)}}"
+                                >
+                            </span>
+                            <span class="sm:col-span-3">
+                                <label class="py-4 block" for="lastname">Last Name</label>
+                                <input
+                                    class="block w-full"
+                                    type="text"
+                                    name="lastname"
+                                    id="lastname"
+                                    value="{{old('lastname', $person->lastname)}}"
+                                >
+                            </span>
+
+                            <span class="sm:col-span-3">
+                                <label class="py-4 block" for="email">Email</label>
+                                <input
+                                    class="block w-full"
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    value="{{old('email', $person->email)}}"
+                                >
+                            </span>
+
+                            <span class="sm:col-span-3">
+                                <label class="py-4 block" for="phone">Phone</label>
+                                <input
+                                    class="block w-full"
+                                    type="text"
+                                    name="phone"
+                                    id="phone"
+                                    value="{{old('phone', $person->phone)}}"
+                                >
+                            </span>
+                        </div>
+
+                        <div class="mt-6 flex items-center justify-end gap-x-6">
+                            <a href="{{ route('person.index') }}" class="">Cancel</a>
+
+                            <button class="bg-indigo-50 py-2 px-3 rounded-full" type="submit">Update</button>
+                        </div>
+                    </form>
+
+                    <form action="{{route('person.destroy', $person->id)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+
+                        <div class="bg-red-200">
+                            <button type="submit">Delete</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PersonController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,17 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::controller(PersonController::class)
+    ->prefix('person')
+    ->name('person')
+    ->middleware('auth')
+    ->group(function () {
+        Route::get('/', 'index')->name('.index');
+        Route::get('/create', 'create')->name('.create');
+        Route::post('/store', 'store')->name('.store');
+        Route::get('/{person}/edit', 'edit')->name('.edit');
+        Route::put('/{person}/update', 'update')->name('.update');
+        Route::delete('/{person}/destroy', 'destroy')->name('.destroy');
+    });
+
+require __DIR__ . '/auth.php';
